67e5b0359b1dd0fd9a52e05f60c8f6d98e5014c8
Jojo1178 <cassenjonathan@gmail.com>
2017-07-14 21:21:33 +0200

    Added placeholder

---
+++ Assets/Scripts/ApplicationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Application Started");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
