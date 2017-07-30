a9be475a747ea7aa2967dc2c16c8a70ce011ed78
y4ka <loris.bezard@hotmail.fr>
2017-07-30 22:23:00 +0200

    [UI] Main Menu Impovements + Scripting
    Main Menu Impovements + Scripting

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void clickButtonPlay()
+    {
+        Debug.Log("BUTTON PLAY CLICKED");
+    }
+
+    public void clickButtonExit()
+    {
+        Debug.Log("BUTTON EXIT CLICKED");
+    }
+}
